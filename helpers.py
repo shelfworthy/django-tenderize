1d1933679fbb64679668d8ec39a2d83bacc367f3
Chris Drackett <chris@chrisdrackett.com>
2010-01-20 22:25:55 -0600

    small updates to multipass for better URL handling.

--- helpers.py
+++ helpers.py
@@ -18,12 +18,14 @@ def tender_api():
         secret      = settings.TENDER_SECRET
     )
 
-def multipass_url(url, user, display_name=None, **kw):
+def multipass_url(url, user, display_name=None, avatar_url=None, extras=None, **kw):
     if user.is_authenticated():
         return tender_api().multipass_url(url, tender_api().multipass(
             username=display_name or user.username,
             email=user.email,
             unique_id=user.id,
+            avatar_url=avatar_url,
+            extras=extras,
             **kw
         ))
     else:
